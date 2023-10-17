9d6579b5f3b75e8f4f997f590e8dd71ed693b92d
Gokhan Yucegok <gyucegok@gmail.com>
2023-10-17 09:51:19 -0400

    v0.1

---
+++ parser.py
@@ -0,0 +1,20 @@
+from google.cloud import storage
+
+def gcs_read(bucket_name, blob_name):
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    with blob.open("r") as f:
+        print(f.read())
+
+    return f
+
+# Untitled-1
+from google.cloud import storage
+
+
+file_content = gcs_read("gyucegok-moodyspoc-test", "test.txt")
+
+print(file_content)
