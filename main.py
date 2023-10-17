0be1a2daf98a6a08de85d31875c468ebbf2f360f
Gokhan Yucegok <gyucegok@gmail.com>
2023-10-17 20:44:20 +0000

    v0.3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from flask import Flask, Response
 from google.cloud import storage
 # from google.cloud import pubsub_v1
@@ -7,13 +8,30 @@ def gcs_read(bucket_name, blob_name):
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
-
     blob = bucket.blob(blob_name)
+
     with blob.open("r") as f:
         returntext = f.read()
         print(returntext)
-
     return returntext
+
+from google.cloud import storage
+
+
+def gcs_write(bucket_name, blob_name, content):
+    storage_client = storage.Client()
+
+    blob_name = blob_name + "." + str(time.time_ns())
+    
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    
+    with blob.open("w") as f:
+        f.write(content)
+
 
 
 file_content = gcs_read("gyucegok-moodyspoc-test", "hello.txt")
@@ -22,7 +40,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return file_content
+    gcs_write("gyucegok-moodyspoc-test2", "hello.txt", file_content)
+    return file_content + "   Epoch: " + str(time.time_ns())
 
 if __name__ == "__main__":
     print(" Starting app...")
