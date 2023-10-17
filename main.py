257d3ffcef136595523b28b18d0d084cb187caa7
Gokhan Yucegok <gyucegok@gmail.com>
2023-10-17 19:54:48 +0000

    v0.2

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import os
 from flask import Flask, Response
-# from google.cloud import storage
+from google.cloud import storage
 # from google.cloud import pubsub_v1
 
-""" def gcs_read(bucket_name, blob_name):
+def gcs_read(bucket_name, blob_name):
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
+
     blob = bucket.blob(blob_name)
-
     with blob.open("r") as f:
-        print(f.read())
+        returntext = f.read()
+        print(returntext)
 
-    return f
+    return returntext
 
-file_content = gcs_read("gyucegok-moodyspoc-test", "hello.txt") """
 
+file_content = gcs_read("gyucegok-moodyspoc-test", "hello.txt")
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
- #   return file_content
-    return f"hello!!!"
+    return file_content
 
 if __name__ == "__main__":
     print(" Starting app...")
